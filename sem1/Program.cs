b5740fac73846b87b6b9c832cee64fc1a6f0039c
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-06-23 14:19:28 +0300

    first commit: home task 1

---
+++ sem1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+int a = 5, b = 7, max_num, min_num;
+if (a > b) {
+   max_num = a; min_num = b;
+} else
+{
+    max_num = b; min_num = a;
+};
+Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
+
+
+a = 2; b = 10;
+if (a > b) {
+   max_num = a; min_num = b;
+} else
+{
+    max_num = b; min_num = a;
+};
+Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
+
+a = -9; b = -3;
+if (a > b) {
+   max_num = a; min_num = b;
+} else
+{
+    max_num = b; min_num = a;
+};
+Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
