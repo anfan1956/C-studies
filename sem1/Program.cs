89da78b0731e15b680a8217e8bc64412c8f53a35
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-06-23 14:41:52 +0300

    home task 2

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -5,14 +5,17 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-int a = 5, b = 7, max_num, min_num;
+
+Console.WriteLine("");
+
+int a = 5, b = 7, max_num, min_num, c;
 if (a > b) {
    max_num = a; min_num = b;
 } else
 {
     max_num = b; min_num = a;
 };
-Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
+Console.WriteLine("task 1, max number: " + max_num + ", min number: " + min_num);
 
 
 a = 2; b = 10;
@@ -22,7 +25,7 @@ if (a > b) {
 {
     max_num = b; min_num = a;
 };
-Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
+Console.WriteLine("task 1, max number: " + max_num + ", min number: " + min_num);
 
 a = -9; b = -3;
 if (a > b) {
@@ -31,4 +34,30 @@ if (a > b) {
 {
     max_num = b; min_num = a;
 };
-Console.WriteLine("max number: " + max_num + ", min number: " + min_num);
+Console.WriteLine("task 1, max number: " + max_num + ", min number: " + min_num);
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*
+*/
+
+Console.WriteLine("");
+a =2; b =3; c = 7;
+max_num = a;
+if (b > max_num) max_num = b;
+if (c > max_num) max_num = c;
+Console.WriteLine("task 4, max number: " + max_num );
+
+a =45; b =5; c = 78;
+max_num = a;
+if (b > max_num) max_num = b;
+if (c > max_num) max_num = c;
+Console.WriteLine("task 4, max number: " + max_num );a =45; b =5; c = 78;
+
+a =22; b =3; c = 9;
+max_num = a;
+if (b > max_num) max_num = b;
+if (c > max_num) max_num = c;
+Console.WriteLine("task 4, max number: " + max_num );
